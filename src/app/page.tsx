103b376c22b70a014e1d4e3f2e109912eea2107e
Mathias Klenk <16352316+maakle@users.noreply.github.com>
2023-11-28 10:48:00 +0100

    Prevented to fill out all values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { FileDropArea } from "@/components/FileDropArea";
 import { TagInput } from "@/components/TagInput";
 import { generateDocument, getTags } from "@/lib/docxtemplater";
+import { isEmpty } from "ramda";
 import { ChangeEvent, useState } from "react";
 
 export default function Home() {
@@ -20,13 +21,30 @@ export default function Home() {
     setTags((tags) => ({ ...tags, [id]: value }));
   };
 
+  const processTags = () => {
+    const missingValue = [];
+
+    for (const key in tags) {
+      if (isEmpty(tags[key])) {
+        missingValue.push(key);
+      }
+    }
+
+    if (missingValue.length > 0) {
+      alert(`Please fill in the following tags: ${missingValue.join(", ")}`);
+      return;
+    }
+
+    generateDocument(file, tags);
+  };
+
   return (
     <main className="flex bg-stone-100 dark:bg-zinc-900 min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="mt-8 max-w-xl mx-auto px-8">
           <h1 className="text-center">
             <span className="block text-5xl font-bold leading-none">
-              Docxtemplater Demo
+              Replace tags in Docx Demo
             </span>
           </h1>
           <div className="mt-12">
@@ -52,7 +70,7 @@ export default function Home() {
               </div>
               <div className="mt-8">
                 <button
-                  onClick={() => generateDocument(file, tags)}
+                  onClick={processTags}
                   className="inline-block bg-gray-900 dark:bg-white hover:bg-gray-800 text-white dark:text-black dark:hover:bg-gray-100 font-medium rounded-lg px-6 py-4 leading-tight"
                 >
                   Generate document
--- src/components/TagInput.tsx
+++ src/components/TagInput.tsx
@@ -1,21 +1,36 @@
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 interface TagInput {
   tag: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export function TagInput({ tag, onChange }: TagInput) {
+  const [value, setValue] = useState<string>("");
+
   return (
     <div className="mt-4">
-      <label htmlFor="email" className="block text-sm font-medium leading-6">
-        {tag}
-      </label>
-      <div className="mt-2">
+      <label className="block text-sm font-medium leading-6">{tag}</label>
+
+      <div className="relative mt-2 rounded-md shadow-sm">
         <input
           id={tag}
           className="block w-full rounded-md border-0 py-1.5 px-2 dark:bg-neutral-900 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder={tag}
-          onChange={onChange}
+          aria-invalid="true"
+          placeholder="Please enter..."
+          onChange={(e) => {
+            setValue(e.target.value);
+            onChange(e);
+          }}
         />
+        {!value && (
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <ExclamationCircleIcon
+              className="h-5 w-5 text-red-400"
+              aria-hidden="true"
+            />
+          </div>
+        )}
       </div>
     </div>
   );
