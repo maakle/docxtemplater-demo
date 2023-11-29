c697dc38dbe0ae96415342068382bbf68429c473
Mathias Klenk <16352316+maakle@users.noreply.github.com>
2023-11-29 18:22:23 +0100

    Remove package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,16 +90,7 @@ export default function Home() {
                           width="100%"
                           height="600"
                           src={`https://docs.google.com/gview?url=${fileUrl}&embedded=true`}
-                        ></iframe>
-
-                        // <DocViewer
-                        //   pluginRenderers={DocViewerRenderers}
-                        //   documents={[
-                        //     {
-                        //       uri: fileUrl,
-                        //     },
-                        //   ]}
-                        // />
+                        />
                       )}
                     </div>
                   </div>
